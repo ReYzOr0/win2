04bd02e7ff35ab7bc3c6bb705127bb5d9fea10d5
rustem <3123@.ru>
2023-09-13 14:38:28 +0300

    final

---
+++ add.js
@@ -0,0 +1,13 @@
+
+var X = +prompt("Введите первое чило");
+
+var Y = +prompt("Введите второе чило");
+
+var Z = +prompt("Введите третье чило");
+
+var finish = ((X + Y + Z));
+
+var fin = ((finish / 3));
+
+alert("Среднее арифметическое:" + fin)
+
